23c36e3fa0e888a9927bdea07cf96a01e79b8deb
khoilvgcc210040 <Khoilvgcc210040@fpt.edu.vn>
2023-06-19 16:12:20 +0700

    first commitss

---
+++ components/admin_header.php
@@ -0,0 +1,38 @@
+<?php
+   if(isset($message)){
+      foreach($message as $message){
+         echo '
+         <div class="message">
+            <span>'.$message.'</span>
+            <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
+         </div>
+         ';
+      }
+   }
+?>
+
+<header class="header">
+
+   <section class="flex">
+
+      <a href="../admin/dashboard.php" class="logo">Admin<span>Panel</span></a>
+
+      <div class="icons">
+         <div id="menu-btn" class="fas fa-bars"></div>
+         <div id="user-btn" class="fas fa-user"></div>
+      </div>
+
+      <div class="profile">
+         <?php
+            $select_profile = $conn->prepare("SELECT * FROM `staffs` WHERE id = ?");
+            $select_profile->execute([$user_id]);
+            $fetch_profile = $select_profile->fetch(PDO::FETCH_ASSOC);
+         ?>
+         <p><?= $fetch_profile['name']; ?></p>
+         <a href="../home.php" class="btn">return shop</a>
+         <a href="../components/admin_logout.php" class="delete-btn" onclick="return confirm('logout from the website?');">logout</a> 
+      </div>
+
+   </section>
+
+</header>
---
+++ components/connect.php
@@ -0,0 +1,9 @@
+<?php
+
+$db_name = 'mysql:host=nnsgluut5mye50or.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=kuap6j5nk5f1h455';
+$user_name = 'hbbnun5v9y0xgldk';
+$user_password = 'rtxhae3quo4cngk2';
+
+$conn = new PDO($db_name, $user_name, $user_password);
+
+?>
